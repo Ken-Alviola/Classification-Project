4cb0f9eddc93ad9e2086b298c86051e7af2b8f0d
Ken-Alviola <kenalviola@gmail.com>
2021-03-05 09:46:02 -0600

    initial commit

---
+++ acquire.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import os
+
+from env import host, user, password
+
+def get_connection(db, user=user, host=host, password=password):
+    '''
+    This function uses my info from my env file to
+    create a connection url to access the Codeup db.
+    '''
+    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
